97b4376605cb03cc1e8965b8454af6bde87ca006
Psng Abu <abupsngdscz@gmail.com>
2024-02-18 09:33:46 +0530

    added messageContainer messages and message

---
+++ client/src/components/messages/Message.jsx
@@ -0,0 +1,25 @@
+
+const Message = () => {
+    return (
+        <div className="chat chat-end">
+            <div>
+                <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="chat-bubble text-white bg-blue-500 ">
+                hi whats app
+            </div>
+
+            <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+                12.45
+            </div>
+
+        </div>
+    )
+}
+
+export default Message
---
+++ client/src/components/messages/Messages.jsx
@@ -0,0 +1,16 @@
+import Message from './Message'
+
+const messages = () => {
+    return (
+        <div className='px-4 flex overflow-auto'>
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+        </div>
+    )
+}
+
+export default messages
